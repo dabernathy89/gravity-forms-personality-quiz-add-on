deb5fc7d5d187ec915d024f51ff4ab18067d3300
Daniel Abernathy <dabernathy89@gmail.com>
2014-09-29 18:12:11 -0400

    bump version number

--- class-gravity-forms-personality-quiz-addon.php
+++ class-gravity-forms-personality-quiz-addon.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gravity Forms Personality Quiz Add-On
 Description: Create personality quizzes with Gravity Forms.
-Version: 0.1
+Version: 0.2
 Author: Daniel Abernathy
 Author URI: http://www.danielabernathy.com
 License: GPLv3
