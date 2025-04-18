f5ac57ceecbeaaf65c1797c4c793019426636ec9
Daniel Abernathy <daniel@danielabernathy.com>
2025-04-18 12:57:04 -0500

    Update gravity-forms-personality-quiz-addon.php
    remove personal email

--- gravity-forms-personality-quiz-addon.php
+++ gravity-forms-personality-quiz-addon.php
@@ -16,7 +16,7 @@ Author: Daniel Abernathy
 Author URI: http://www.danielabernathy.com
 License: GPLv3
 
-	Copyright 2014 Daniel Abernathy (email : dabernathy89@gmail.com)
+	Copyright 2014 Daniel Abernathy (email : daniel@danielabernathy.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 3, as
